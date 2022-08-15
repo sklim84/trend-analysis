56eb1d9d2ba4c68af05fbcb5a35633a727d9243d
sklim84 <captiong84@gmail.com>
2022-08-15 22:38:13 +0900

    jpss pre-processing 방식 변경(treform → naive NLTK)

--- _datasets/jpss_data.py
+++ _datasets/jpss_data.py
@@ -3,7 +3,9 @@ import pickle
 import re
 
 import pandas as pd
-import treform as ptm
+from nltk import pos_tag
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
 
 
 ####################
@@ -13,14 +15,43 @@ import treform as ptm
 # - 분석대상 : abstract
 ####################
 
+def _preprocess_text(txt):
+    here = pathlib.Path(__file__).resolve().parent
+    loc_stopwords = here / 'stopwordsEng.txt'
+
+    txt = txt.lower()
+    txt = re.sub('[^A-Za-z0-9가-힣_ ]+', '', txt)
+
+    # tokenize
+    words = word_tokenize(txt)
+
+    # stopwords filtering
+    stopwords_def = stopwords.words('english')  # nltk default stopwords
+    stopwords_ctm = [word.strip() for word in open(loc_stopwords, encoding='utf-8')]
+    stopwords_def.extend(stopwords_ctm)
+    words_flt = [word for word in words if word not in stopwords_def]
+
+    # stemming
+    # porter = PorterStemmer()
+    # stemmed = [porter.stem(word) for word in filtered_words]
+    # print(stemmed)
+
+    # select NN*
+    words_tag = pos_tag(words_flt)
+    words_fin = []
+    for word_tag in words_tag:
+        if word_tag[1].startswith('NN'):  # select NN, NNS, ...
+            words_fin.append(word_tag[0])
+
+    return words_fin
+
+
 ####################
 # 데이터 로드 및 전처리
 # - 전처리 : Tokenizing, POS tagging & filtering, n-gram, Stopword Filtering
 ####################
 def load_for_keyword(target_index, reuse_preproc=False):
     here = pathlib.Path(__file__).resolve().parent
-    loc_data = here / 'jpss.csv'
-    loc_stopwords = here / 'stopwordsEng.txt'
 
     # 기전처리된 파일 사용 시
     if reuse_preproc:
@@ -30,30 +61,17 @@ def load_for_keyword(target_index, reuse_preproc=False):
         return documents
 
     # 데이터 로드
-    df_jpss = pd.read_csv(loc_data)
+    df_jpss = pd.read_csv(here / 'jpss.csv')
     target = df_jpss.iloc[:, [target_index]].astype(str).values.tolist()
     # [[document1], ...] → [document1, ...]
     target = sum(target, [])
 
     # 전처리
-    pipeline = ptm.Pipeline(ptm.splitter.NLTK(),
-                            ptm.tokenizer.Word(),
-                            ptm.tagger.NLTK(),
-                            ptm.helper.POSFilter('NN*'),
-                            ptm.helper.SelectWordOnly(),
-                            ptm.ngram.NGramTokenizer(1, 1),
-                            ptm.helper.StopwordFilter(file=loc_stopwords))
-    result = pipeline.processCorpus(target)
-
     documents = []
-    for doc in result:
-        document = ''
-        for sent in doc:
-            new_sent = ' '.join(sent)
-            new_sent = re.sub('[^A-Za-z0-9가-힣_ ]+', '', new_sent)
-            new_sent = new_sent.strip()
-            document += new_sent
-        documents.append(document)
+    for document in target:
+        words = _preprocess_text(document)
+        if len(words) > 0:
+            documents.append(' '.join(words))
 
     # 전처리된 결과 저장
     with open(here / 'jpss_pp_for_keyword.pkl', 'wb') as fout:
@@ -62,10 +80,9 @@ def load_for_keyword(target_index, reuse_preproc=False):
 
     return documents
 
+
 def load_for_coword(target_index, reuse_preproc=False):
     here = pathlib.Path(__file__).resolve().parent
-    loc_data = here / 'jpss.csv'
-    loc_stopwords = here / 'stopwordsEng.txt'
 
     # 기전처리된 파일 사용 시
     if reuse_preproc:
@@ -75,30 +92,19 @@ def load_for_coword(target_index, reuse_preproc=False):
         return documents
 
     # 데이터 로드
-    df_jpss = pd.read_csv(loc_data)
+    df_jpss = pd.read_csv(here / 'jpss.csv')
     target = df_jpss.iloc[:, [target_index]].astype(str).values.tolist()
     # [[document1], ...] → [document1, ...]
     target = sum(target, [])
 
     # 전처리
-    pipeline = ptm.Pipeline(ptm.splitter.NLTK(),
-                            ptm.tokenizer.Word(),
-                            ptm.tagger.NLTK(),
-                            ptm.helper.POSFilter('NN*'),
-                            ptm.helper.SelectWordOnly(),
-                            ptm.ngram.NGramTokenizer(1, 1),
-                            ptm.helper.StopwordFilter(file=loc_stopwords))
-    result = pipeline.processCorpus(target)
-
-    # 구조 변경 : Sentence co-occurrence word를 찾기 위해 하나의 setence를 하나의 document로 변경
     documents = []
-    for doc in result:
-        for sent in doc:
-            new_sent = ' '.join(sent)
-            new_sent = re.sub('[^A-Za-z0-9가-힣_ ]+', '', new_sent)
-            new_sent = new_sent.strip()
-            if len(new_sent) > 0:
-                documents.append(new_sent)
+    for document in target:
+        sentences = sent_tokenize(document)
+        for sentence in sentences:
+            words = _preprocess_text(sentence)
+            if len(words) > 0:
+                documents.append(' '.join(words))
 
     # 전처리된 결과 저장
     with open(here / 'jpss_pp_for_coword.pkl', 'wb') as fout:
@@ -107,32 +113,32 @@ def load_for_coword(target_index, reuse_preproc=False):
 
     return documents
 
+
 ####################
 # Term Weighting을 위한 데이터 로드 및 처리
 # - 전처리 : Tokenizing, POS tagging & filtering, Stopword Filtering
 ####################
 def load_for_term_weighting(label_index, target_index):
     here = pathlib.Path(__file__).resolve().parent
-    loc_data = here / 'jpss.csv'
-    loc_stopwords = here / 'stopwordsEng.txt'
 
-    # 데이터 로드
-    label = ptm.CorpusFromCSVFile(loc_data, label_index)
-    content = ptm.CorpusFromCSVFile(loc_data, target_index)
+    df_jpss = pd.read_csv(here / 'jpss.csv')
+    label = df_jpss.iloc[:, label_index].astype(str).values.tolist()
+    target = df_jpss.iloc[:, [target_index]].astype(str).values.tolist()
+    # [[document1], ...] → [document1, ...]
+    target = sum(target, [])
+
     # 전처리
-    pipeline = ptm.Pipeline(ptm.splitter.NLTK(),
-                            ptm.tokenizer.Word(),
-                            ptm.tagger.NLTK(),
-                            ptm.helper.POSFilter('NN*'),
-                            ptm.helper.SelectWordOnly(),  # 품사 태크 제거
-                            ptm.helper.StopwordFilter(file=loc_stopwords))
-    return label.docs[1:], pipeline.processCorpus(content.docs[1:])
+    documents = []
+    for document in target:
+        words = _preprocess_text(document)
+        if len(words) > 0:
+            documents.append(' '.join(words))
+
+    return label, documents
 
 
 def load_for_topic(timestamp_index, target_index, reuse_preproc=False):
     here = pathlib.Path(__file__).resolve().parent
-    loc_data = here / 'jpss.csv'
-    loc_stopwords = here / 'stopwordsEng.txt'
 
     # 기전처리된 파일 사용 시
     if reuse_preproc:
@@ -143,31 +149,18 @@ def load_for_topic(timestamp_index, target_index, reuse_preproc=False):
         return timestamps, documents
 
     # 데이터 로드
-    df_jpss = pd.read_csv(loc_data)
-    timestamps = df_jpss.iloc[:, timestamp_index].tolist()
+    df_jpss = pd.read_csv(here / 'jpss.csv')
+    timestamps = df_jpss.iloc[:, timestamp_index].astype(str).values.tolist()
     target = df_jpss.iloc[:, [target_index]].astype(str).values.tolist()
     # [[document1], ...] → [document1, ...]
     target = sum(target, [])
 
     # 전처리
-    pipeline = ptm.Pipeline(ptm.splitter.NLTK(),
-                            ptm.tokenizer.Word(),
-                            ptm.tagger.NLTK(),
-                            ptm.helper.POSFilter('NN*'),
-                            ptm.helper.SelectWordOnly(),  # 품사 태크 제거
-                            ptm.helper.StopwordFilter(file=loc_stopwords))
-    result = pipeline.processCorpus(target)
-
     documents = []
-    for doc in result:
-        document = []
-        for sent in doc:
-            for word in sent:
-                new_word = re.sub('[^A-Za-z0-9가-힣_ ]+', '', word)
-                new_word = new_word.strip()
-                if len(new_word) > 0:
-                    document.append(new_word)
-        documents.append(document)
+    for document in target:
+        words = _preprocess_text(document)
+        if len(words) > 0:
+            documents.append(words)
 
     # 전처리된 결과 저장
     with open(here / 'jpss_pp_for_topic.pkl', 'wb') as fout:
@@ -180,8 +173,6 @@ def load_for_topic(timestamp_index, target_index, reuse_preproc=False):
 
 def load_for_bertopic(timestamp_index, target_index, reuse_preproc=False):
     here = pathlib.Path(__file__).resolve().parent
-    loc_data = here / 'jpss.csv'
-    loc_stopwords = here / 'stopwordsEng.txt'
 
     # 기전처리된 파일 사용 시
     if reuse_preproc:
@@ -192,31 +183,18 @@ def load_for_bertopic(timestamp_index, target_index, reuse_preproc=False):
         return timestamps, documents
 
     # 데이터 로드
-    df_jpss = pd.read_csv(loc_data)
+    df_jpss = pd.read_csv(here / 'jpss.csv')
     timestamps = df_jpss.iloc[:, timestamp_index].tolist()
     target = df_jpss.iloc[:, [target_index]].astype(str).values.tolist()
     # [[document1], ...] → [document1, ...]
     target = sum(target, [])
 
     # 전처리
-    pipeline = ptm.Pipeline(ptm.splitter.NLTK(),
-                            ptm.tokenizer.Word(),
-                            ptm.tagger.NLTK(),
-                            ptm.helper.POSFilter('NN*'),
-                            ptm.helper.SelectWordOnly(),  # 품사 태크 제거
-                            ptm.helper.StopwordFilter(file=loc_stopwords))
-    result = pipeline.processCorpus(target)
-
     documents = []
-    for doc in result:
-        document = ''
-        for sent in doc:
-            for word in sent:
-                new_word = re.sub('[^A-Za-z0-9가-힣_ ]+', '', word)
-                new_word = new_word.strip()
-                if len(new_word) > 0:
-                    document = ' '.join([document, new_word])
-        documents.append(document)
+    for document in target:
+        words = _preprocess_text(document)
+        if len(words) > 0:
+            documents.append(' '.join(words))
 
     # 전처리된 결과 저장
     with open(here / 'jpss_pp_for_bert.pkl', 'wb') as fout:
